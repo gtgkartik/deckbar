8dbfc6924173e55b98a55a3430b9c2c60b83d185
chandrasaripaka <chandru.chanda@gamil.com>
2023-05-24 18:46:56 +0800

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -11,16 +11,28 @@ const AboutUs = () => {
                 <div className="font-libre flex flex-col md:px-12 md:ml-20 sm:mx-10 justify-center">
                     <div  className="text-3xl  font-semibold">About Us</div>
                     <div className="py-5 mt-4 leading-7">
-                        Bluemist is a local gastro-bar that has been serving the finest coffee and a wide range of top quality alcoholic beverages at the hub of Tanjong Pagar since 2002. Our food menu is meticulously crafted to match the list of beverages that some dishes are a fusion.
+                        Welcome to DeckBar, where we blend the elements of relaxation, entertainment, and exceptional service to create a truly memorable experience. Located at CTHUB, DeckBar is a vibrant and dynamic destination that caters to groups and individuals seeking a unique social atmosphere with amazing conversations in Alfresco model.
                     </div>
                     <div className="py-5 leading-7">
-                        We once sat on the ground level of Amara Shopping Center (current Amara 100AM) that can be spotted by a misty reflective blue light from all the window panels. It was a “love at first sight”, thus the name Bluemist. In 2010, due to the refurbishment of Amara Shopping Center, Bluemist was relocated to Blk 7 Tanjong Pagar Plaza, #01-103 untill today.
+                        Welcome to our Deck Bar, where delightful drinks and delectable eats meet in a harmonious blend of flavors and ambiance. Nestled in a vibrant and picturesque setting, our bar offers an enchanting outdoor experience, allowing you to savor the finest culinary creations while enjoying the beauty of nature.
                     </div>
                     <div className="py-5 leading-7">
-                        We once sat on the ground level of Amara Shopping Center (current Amara 100AM) that can be spotted by a misty reflective blue light from all the window panels. It was a “love at first sight”, thus the name Bluemist. In 2010, due to the refurbishment of Amara Shopping Center, Bluemist was relocated to Blk 7 Tanjong Pagar Plaza, #01-103 untill today.
+                        At our Deck Bar, we believe that great food and drinks are meant to be savored in the open air, where the gentle breeze and warm sunshine enhance the dining experience. Whether you're seeking a refreshing cocktail, a perfectly crafted brew, or a tantalizing culinary adventure, we have something to satisfy every palate.
+                    </div>
+                    <div className="py-5 leading-7">
+                        Complementing our impressive beverage selection is our carefully curated menu, designed to delight and satisfy. Our culinary team takes inspiration from global flavors, infusing traditional dishes with a modern twist. Whether you're craving mouthwatering tapas, artisanal pizzas, or succulent grilled specialties, our menu offers a diverse range of options to please every palate.
+                    </div>
+                    <div className="py-5 leading-7">
+                        As you unwind and enjoy your meal, immerse yourself in the lively atmosphere of our Deck Bar. Surrounded by lush greenery, twinkling lights, and the sounds of laughter and conversation, you'll feel transported to a world of relaxation and enjoyment. Our friendly and attentive staff is always ready to ensure your experience is nothing short of exceptional, providing warm hospitality and personalized service.
+                        </div>
+                    <div className="py-5 leading-7">
+                        Whether you're meeting friends for happy hour, enjoying a romantic evening under the stars, or celebrating a special occasion, our Deck Bar is the perfect destination. Join us for a memorable dining experience where the flavors of exquisite drinks and mouthwatering eats are elevated by the enchanting ambiance of the great outdoors.
+                    </div>
+                    <div className="py-5 leading-7">
+                        Come and discover the true essence of alfresco dining at our bar—a place where flavors mingle, conversations flow, and unforgettable memories are made. We look forward to welcoming you to our Deck Bar, where every sip and every bite is a celebration of the good life.
                     </div>
                     <div className="py-10 leading-7">
-                    For reservation of tables, WhatsApp us at +65 8771 2549
+                        For reservation of tables, WhatsApp us at +65 9887 3377
                     </div>
                     <div className="">
                         <button className="rounded-sm bg-black hover:bg-yellow text-white px-7 py-2">Menu</button>
