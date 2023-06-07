3286f73ba8513f891946089db8742ec375d06e22
kartik kumar <gtgkartik@gmail.com>
2023-06-07 15:40:25 +0530

    Fetched data from instagram

--- src/pages/InstaFeeds.js
+++ src/pages/InstaFeeds.js
@@ -1,44 +1,48 @@
-import React, { useState, useEffect, useRef } from 'react'
-import axios from 'axios'
-import Feed from './Feed'
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import Feed from "./Feed";
 
-const InstaFeeds = ({token, ...props}) => {
-    const [feeds, setFeedsData] = useState([])
-    //use useRef to store the latest value of the prop without firing the effect
-    const tokenProp = useRef(token);
-    tokenProp.current = token;
-    useEffect(() => {
-        // this is to avoid memory leaks
-        const abortController = new AbortController();
+const InstaFeeds = ({ token, ...props }) => {
+  const [feeds, setFeedsData] = useState([]);
+  //use useRef to store the latest value of the prop without firing the effect
+  const tokenProp = useRef(token);
+  tokenProp.current = token;
+  useEffect(() => {
+    // this is to avoid memory leaks
+    const abortController = new AbortController();
 
-        async function fetchInstagramPost () {
-          try{
-            axios
-                .get(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${props.limit}&access_token=${token}`)
-                .then((resp) => {
-                    setFeedsData(resp.data.data)
-                })
-          } catch (err) {
-              console.log('error', err)
-          }
-        }
+    async function fetchInstagramPost() {
+      try {
+        axios
+          .get(
+            `https://v1.nocodeapi.com/gtgkartik/instagram/ModAOqKHQVNQwshl`
+          )
+          .then((resp) => {
+            setFeedsData(resp.data.data);
+          });
+      } catch (err) {
+        console.log("error", err);
+      }
+    }
+
+    // manually call the fecth function
+    fetchInstagramPost();
+
+    return () => {
+      // cancel pending fetch request on component unmount
+      abortController.abort();
+    };
+  }, [props.limit]);
+
+  return (
+    <div class=" grid md:grid-cols-4 sm:grid-cols-1 ls:grid-cols-3 gap-2 mx-auto mt-[100px] md:px-20 ls:px-15 sm:px-10">
+      {feeds.map((feed) => (
         
-        // manually call the fecth function 
-        fetchInstagramPost();
-  
-        return () => {
-            // cancel pending fetch request on component unmount
-            abortController.abort(); 
-        };
-    }, [props.limit])
-
-    return (
-        <div className="container">
-            {feeds.map((feed) => (
-                <Feed key={feed.id} feed={feed} />
-            ))}
-        </div>
-    );
-}
+          <Feed key={feed.id} feed={feed} />
+        
+      ))}
+    </div>
+  );
+};
 
 export default InstaFeeds;
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,10 +1,16 @@
-import MenuCard from "../components/MenuCard";
 import InstaFeeds from './InstaFeeds.js'
+import Header from "../components/Header";
+import HeaderImage from "../components/HeaderImage";
 
 const Test = () => {
   return (
     <>
-           <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12} />
+        <Header activities="text-yellow" />
+      <HeaderImage
+        title="Activities"
+        imageurl="https://images.unsplash.com/photo-1612325508365-22caba7bb69e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+      />
+        <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12} />
     </>
   );
 };
