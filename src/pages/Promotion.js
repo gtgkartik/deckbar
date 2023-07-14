dad3d140007026c57eaef9ed681f1b4e55bf288e
kartik kumar <gtgkartik@gmail.com>
2023-07-14 22:15:27 +0530

    Made changes to the promotions

--- src/pages/Promotion.js
+++ src/pages/Promotion.js
@@ -2,6 +2,15 @@ import Header from "../components/Header";
 import HeaderImage from "../components/HeaderImage";
 import image1 from '../Images/originalimages/1.jpg'
 import image2 from '../Images/originalimages/2.jpg'
+import image3 from '../Images/originalimages/3.jpg'
+import image4 from '../Images/originalimages/4.jpg'
+import image5 from '../Images/originalimages/5.jpg'
+import image6 from '../Images/originalimages/6.jpg'
+import image7 from '../Images/originalimages/7.jpg'
+import image8 from '../Images/originalimages/8.jpg'
+import image9 from '../Images/originalimages/9.jpg'
+import image10 from '../Images/originalimages/10.jpg'
+
 import { motion } from "framer-motion"
 
 const Promotion = () => {
@@ -14,43 +23,81 @@ const Promotion = () => {
         imageurl="https://images.unsplash.com/photo-1572116469696-31de0f17cc34?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
       />
     {/* Gallery  */}
-    <div class=" grid md:grid-cols-4 sm:grid-cols-1 ls:grid-cols-3 gap-2 mx-auto mt-[100px] md:px-20 ls:px-15 sm:px-10">
+    <div class=" grid md:grid-cols-4 sm:grid-cols-1 ls:grid-cols-3 gap-5 mx-auto mt-[100px] md:px-20 ls:px-15 sm:px-10">
         <div class="w-full rounded">
           <img
             src= {image1}
             alt="image"
-          />
-        </div>
-        <div class=" rounded">
-          <img
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-            alt="image"
+            className="w-full h-full object-cover rounded-md"
           />
         </div>
         <div class="w-full rounded">
           <img
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            src= {image2}
             alt="image"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </div>
+        <div class="w-full  object-cover rounded">
+          <img
+            src= {image3}
+            alt="image"
+            className="w-full h-full object-cover rounded-md"
           />
         </div>
         <div class="w-full rounded">
           <img
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            src= {image4}
             alt="image"
+            className="w-full h-full object-cover rounded-md"
           />
         </div>
         <div class="w-full rounded">
           <img
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            src= {image5}
             alt="image"
+            className="w-full h-full object-cover rounded-md"
           />
         </div>
         <div class="w-full rounded">
           <img
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            src= {image6}
             alt="image"
+            className="w-full h-full object-cover rounded-md"
           />
         </div>
+        <div class="w-full rounded">
+          <img
+            src= {image7}
+            alt="image"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </div>
+        <div class="w-full rounded">
+          <img
+            src= {image8}
+            alt="image"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </div>
+
+        <div class="w-full rounded">
+          <img
+            src= {image9}
+            alt="image"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </div>
+
+        <div class="w-full rounded">
+          <img
+            src= {image10}
+            alt="image"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </div>
+        
+ 
       </div>
 
       </motion.div>
