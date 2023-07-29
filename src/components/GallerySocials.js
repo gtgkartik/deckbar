a9653b0d840639ae8f1f90de014a38beea5c6db8
kartik kumar <gtgkartik@gmail.com>
2023-07-29 19:59:56 +0530

    made final changes to home images

--- src/components/GallerySocials.js
+++ src/components/GallerySocials.js
@@ -1,34 +1,53 @@
+
+import image1 from '../Images/homeimages/25.jpg'
+import image2 from '../Images/homeimages/26.jpg'
+import image3 from '../Images/homeimages/27.jpg'
+import image4 from '../Images/homeimages/28.jpg'
+import image5 from '../Images/homeimages/29.jpg'
+import image6 from '../Images/homeimages/30.jpg'
+import image7 from '../Images/homeimages/31.jpg'
+import image8 from '../Images/homeimages/32.jpg'
+import image9 from '../Images/homeimages/33.jpg'
+import image10 from '../Images/homeimages/34.jpg'
+import image11 from '../Images/homeimages/35.jpg'
+
+
+
+
+
+
+
 const GallerySocials = () => {
     return ( 
         <>
             
 <div className="grid grid-cols-2 md:grid-cols-3 gap-4 sm:mx-[30px] sm:flex-col ls:mx-[100px] md:mx-[150px] mt-[60px]">
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <img className="h-auto max-w-full rounded-lg" src={image1} alt="" />
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/1051399/pexels-photo-1051399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image2} alt=""/>
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/2474658/pexels-photo-2474658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image3} alt=""/>
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/1117862/pexels-photo-1117862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image4} alt=""/>
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/941869/pexels-photo-941869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image5} alt=""/>
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/958546/pexels-photo-958546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image6} alt=""/>
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/209540/pexels-photo-209540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image11} alt=""/>
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image8} alt=""/>
     </div>
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/958546/pexels-photo-958546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+        <img className="h-auto max-w-full rounded-lg" src={image9} alt=""/>
     </div>
 
 </div>
