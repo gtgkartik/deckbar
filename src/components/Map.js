bb22d83483ea5b0778bfe3e03c8d568e68ef62a4
kartik kumar <gtgkartik@gmail.com>
2023-07-31 16:38:02 +0530

    changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ import "./Map.css";
 
 export default function Map() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_API_KEY_GOOGLE,
+    googleMapsApiKey: "AIzaSyAaiF8LlG6Bb9nIr3hL1wMHR9_PY87WNVs",
   });
 
   if (!isLoaded) return <div>Loading...</div>;
