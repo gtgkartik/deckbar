a043067e3667e2948a0c3e67d50386fecf8d6410
kartik kumar <gtgkartik@gmail.com>
2023-07-31 16:28:19 +0530

    secured google maps api keys changes also made in MAP COMP

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ import "./Map.css";
 
 export default function Map() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    googleMapsApiKey: process.env.REACT_APP_API_KEY_GOOGLE,
   });
 
   if (!isLoaded) return <div>Loading...</div>;
