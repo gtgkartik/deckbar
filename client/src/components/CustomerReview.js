d98cc75fd8c95feafa784ecd34fa2584a129d718
kartik kumar <gtgkartik@gmail.com>
2023-08-06 14:58:46 +0530

    localhost

--- client/src/components/CustomerReview.js
+++ client/src/components/CustomerReview.js
@@ -7,7 +7,7 @@ const [reviews, setReviews] = useState([]);
 useEffect(() => {
   // Fetch reviews data from the backend API
   axios
-    .get('https://master.d20py1fbr335jq.amplifyapp.com/api/reviews')
+    .get('http://localhost:5000/api/reviews')
     .then((response) => {
       setReviews(response.data);
     })
