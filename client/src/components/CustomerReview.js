134fe175571c89c747818b75e6dbe7676ff957d0
kartik kumar <gtgkartik@gmail.com>
2023-08-10 16:02:06 +0530

    changes

--- client/src/components/CustomerReview.js
+++ client/src/components/CustomerReview.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Amplify from ''
 
 const CustomerReview = () => {
 const [reviews, setReviews] = useState([]);
